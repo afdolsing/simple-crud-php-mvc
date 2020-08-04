82f67e5c4f436ca4e23ad392f06ad487fa0dd206
afdolsing <singgih.student@gmail.com>
2020-08-04 20:27:23 +0700

    update model

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -7,6 +7,4 @@ class User_model{
     {
         return $this->nama;
     }
-
-
 }
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,5 @@
 <div class="container">
 <h2 class="mt-4">About me</h2>
 <img src="<?= BASE_URL ?>/img/dev.jpg" height="100" alt="developer" class="rounded-circle shadow">
-<p>halo , my name <?= $data['name'] ?> and my job a <?= $data['job'] ?> </p>
+<p>halo , my name <?= $data['name'] ?> and my job is a <?= $data['job'] ?> </p>
 </div>
